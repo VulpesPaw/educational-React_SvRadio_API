0ec08f87445ca3ef2c3e0e7643db1f77d841a65e
VulpesPaw <richard.jh.johansson@gmail.com>
2021-10-10 17:11:18 +0200

    Initial File

---
+++ application/spare_parts/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <>
+            <h2>This is a short about page on the same page as a SPA</h2>
+        </>
+    );
+}
