97222b73128e92979a9b07f472169c85647c2064
VulpesPaw <richard.jh.johanson@gmail.com>
2022-09-14 10:18:25 +0200

    Update About Text

--- application/spare_parts/About.js
+++ application/spare_parts/About.js
@@ -4,6 +4,8 @@ export default function About() {
     return (
         <>
             <h2 className='center_text'>This is a short about page on the same page as a SPA</h2>
+            <h2 className='center_text'>AKA. the page never reloaded AND you get a new URL </h2>
+
         </>
     );
 }
